98aa83946a20c5eb7d1168bb1be2483425be887d
lost-hope <s.willrodt@yahoo.de>
2023-01-30 19:02:13 +0100

    Changed to a global deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -21,8 +21,8 @@ const rest = new REST({ version: '10' }).setToken(token);
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
+        await rest.put(
+            Routes.applicationCommands(clientId),
             { body: commands },
         );
 
