97ba2d2f0c98e1d147ea7dcf167bc521bc5ee356
lost-hope <s.willrodt@yahoo.de>
2023-02-05 00:42:29 +0100

    fixed typo

--- commands/levelshifter.js
+++ commands/levelshifter.js
@@ -3,11 +3,11 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('shift')
-        .setDescription('Find reccomended levelshifters'),
+        .setDescription('Find recommended levelshifters'),
     async execute(interaction) {
         const exampleEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
-            .setTitle('Level Shifters')
+            .setTitle('Levelshifters')
             .setURL('https://kno.wled.ge/basics/compatible-hardware/#levelshifters')
             .setThumbnail('https://raw.githubusercontent.com/Aircoookie/WLED/master/images/wled_logo_akemi.png');
         await interaction.reply({ embeds: [exampleEmbed] });
