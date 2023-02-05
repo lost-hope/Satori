efcfe1227fd828c40c03aec86327a9bf4b7f7292
lost-hope <s.willrodt@yahoo.de>
2023-02-05 01:27:32 +0100

    fixed issue with too long message

--- commands/segmentmatrix.js
+++ commands/segmentmatrix.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, Options } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Options, InteractionCollector } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -34,29 +34,53 @@ module.exports = {
         vSize = Math.round(matrixHeight / vNum);
         hSize = Math.round(matrixWidth / hNum);
 
-        let outputJson = "```JS\n{\"on\":true,\"bri\":255,\"transition\":7,\"mainseg\":0,\"seg\":[\n";
-        let outputLayout = "```"
-        index = 0;
-        for (let h = 0; h < hNum; h++) {
-            for (let w = 0; w < vNum; w++) {
-                outputJson += "{\"id\":" + index + ",\"start\":" + w * vSize + ",\"stop\":" + ((w * vSize) + vSize) + ",\"startY\":" + h * hSize + ",\"stopY\":" + +((h * hSize) + hSize) + "},\n"
-                outputLayout += index + " ";
-                index++;
-            }
-            outputLayout += "\n";
-        }
-        outputLayout += "\n```";
-        outputJson += "]}\n```";
-        console.log(outputJson);
-        const exampleEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle('Preset JSON')
-            .addFields(
-                { name: "Matrix Size", value: "" + matrixWidth + "x" + matrixHeight, inline: true },
-                { name: "Segment Size", value: "" + vNum + "x" + hNum, inline: true },
-                { name: "Segment Layout", value: outputLayout, inline: true },
+        if (vNum * hNum > 32) {
+            interaction.reply({ content: "Max segments are 32. You choose " + (vNum * hNum) + " segments", ephemeral: true });
+        } else {
 
-                { name: "Preset JSON", value: outputJson });
-        await interaction.reply({ embeds: [exampleEmbed] });
+            let outputJson = "{\"on\":true,\"bri\":255,\"transition\":7,\"mainseg\":0,\"seg\":[\n";
+            let outputLayout = "```"
+            index = 0;
+            for (let h = 0; h < hNum; h++) {
+                for (let w = 0; w < vNum; w++) {
+                    outputJson += "{\"id\":" + index + ",\"start\":" + w * vSize + ",\"stop\":" + ((w * vSize) + vSize) + ",\"startY\":" + h * hSize + ",\"stopY\":" + +((h * hSize) + hSize) + "},\n"
+                    outputLayout += index + " ";
+                    index++;
+                }
+                outputLayout += "\n";
+            }
+            outputLayout += "\n```";
+            outputJson += "]}\n";
+            let outputField;
+            const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle('Preset JSON')
+                .addFields(
+                    { name: "Matrix Size", value: "" + matrixWidth + "x" + matrixHeight, inline: true },
+                    { name: "Segment Size", value: "" + vNum + "x" + hNum, inline: true },
+                    { name: "Segment Layout", value: outputLayout, inline: true });
+            if (outputJson.length > 1024) {
+                outputJsonSplit = chunkSubstr(outputJson, 1015);
+                console.log(outputJsonSplit);
+                outputJsonSplit.forEach((element, index) => {
+                    exampleEmbed.addFields({ name: "Preset JSON part " + index, value: "```JS\n" + element + "```" });
+                });
+            } else {
+                exampleEmbed.addFields({ name: "Preset JSON", value: "```JS\n" + outputJson + "```" });
+            }
+            console.log(outputJson);
+            await interaction.reply({ embeds: [exampleEmbed] });
+        }
     },
+};
+
+function chunkSubstr(str, size) {
+    const numChunks = Math.ceil(str.length / size)
+    const chunks = new Array(numChunks)
+
+    for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+        chunks[i] = str.substr(o, size)
+    }
+
+    return chunks
 };
